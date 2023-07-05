03f8362177f929468a340451829037c1fbb1c78a
David-Aires <david.airespimentel@student.unamur.be>
2023-07-05 20:55:53 +0200

    change configurations request

--- queries/analytics/stats/getWebsiteStatsConfig.js
+++ queries/analytics/stats/getWebsiteStatsConfig.js
@@ -18,24 +18,30 @@ async function relationalQuery(websiteId, { start_at, end_at, filters = {} }) {
   );
 
   return rawQuery(
-    `select sum(t.c) as "pageviews",
-        count(distinct t.session_id) as "uniques",
-        sum(case when t.c = 1 then 1 else 0 end) as "bounces",
-        sum(t.time) as "totaltime"
-      from (
-        select configuration.session_id,
-          ${getDateQuery('configuration.created_at', 'hour')},
-          count(*) c,
-          ${getTimestampInterval('configuration.created_at')} as "time"
-        from configuration
-          join website 
-            on configuration.website_id = website.website_id
-          ${joinSession}
-        where website.website_uuid = $1${toUuid()}
-          and configuration.created_at between $2 and $3
-          ${sessionQuery}
-        group by 1, 2
-     ) t`,
+    `with data as (select event.event_id, 
+      event.configuration_uuid as "id"
+      ${getTimestampInterval('event.created_at')} as "time",
+      from event
+      group by 1,2),
+    
+    data2 as (select configuration.session_id,
+      ${getDateQuery('configuration.created_at', 'hour')},
+      count(*) c,
+      configuration.configuration_uuid as "id"
+    from configuration
+      join website 
+        on configuration.website_id = website.website_id
+      ${joinSession}
+    where website.website_uuid = $1${toUuid()}
+      and configuration.created_at between $2 and $3
+      ${sessionQuery}
+    group by 1, 2, 4)
+    
+    select sum(data2.c) as "pageviews",
+    count(distinct data2.session_id) as "uniques",
+    sum(case when data2.c = 1 then 1 else 0 end) as "bounces",
+    sum(data.time) as "totaltime"
+    from data join data2 on data.id = data2.id`,
     params,
   );
 }
